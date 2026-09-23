978abb3a9d7c017ad7fb60c3199f5b69c80ee5d5
nitinyadavdsn108 <nitinyadavdsn108@gmail.com>
2026-09-23 23:03:08 +0530

    middle index

---
+++ java_classes/middleIndex.java
@@ -0,0 +1,23 @@
+public class middleIndex {
+    class Solution {
+        public int findMiddleIndex(int[] nums) {
+            long totalSum = 0;
+            for (int num : nums) {
+                totalSum += num;
+            }
+
+            long leftSum = 0;
+            for (int i = 0; i < nums.length; i++) {
+                long rightSum = totalSum - leftSum - nums[i];
+
+                if (leftSum == rightSum) {
+                    return i;
+                }
+
+                leftSum += nums[i];
+            }
+
+            return -1;
+        }
+    }
+}
